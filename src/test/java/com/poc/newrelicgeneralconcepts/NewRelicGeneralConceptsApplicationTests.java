117d6fc8468121b3461b9e2e01a4356ed793259d
Rafiere <rafapgbr@gmail.com>
2023-05-12 09:26:15 -0300

    feat: add new app files

---
+++ src/test/java/com/poc/newrelicgeneralconcepts/NewRelicGeneralConceptsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.poc.newrelicgeneralconcepts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewRelicGeneralConceptsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
